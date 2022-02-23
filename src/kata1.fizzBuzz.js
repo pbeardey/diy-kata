c512445599893fda46fff6c2e6d3d22946e24a7c
Paul Beard <pbeardey@gmail.com>
2022-02-23 20:58:19 +0000

    test written, tests passed

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,3 +1,16 @@
-const fizzBuzz = number => {};
+const fizzBuzz = number => {
+    const isDivisableBy3 = (number % 3) === 0;
+    const isDivisableBy5 = (number % 5) === 0;
+
+    if (isDivisableBy3 && isDivisableBy5){
+        return 'FizzBuzz'
+    } else if (isDivisableBy3) {
+        return 'Fizz';
+    } else if (isDivisableBy5) {
+        return 'Buzz';
+    } else {
+        return number;
+    }
+};
 
 module.exports = fizzBuzz;
--- test/kata1.fizzBuzz.test.js
+++ test/kata1.fizzBuzz.test.js
@@ -1,11 +1,31 @@
 const { fizzBuzz } = require("../src");
 
 describe("fizzBuzz", () => {
-  test("returns Fizz when passed a multiple of 3", () => {});
+  it("returns Fizz when passed a multiple of 3", () => {
+    expect(fizzBuzz(12)).toEqual('Fizz');
+    expect(fizzBuzz(102)).toEqual('Fizz');
+    expect(fizzBuzz(3003)).toEqual('Fizz');
+    expect(fizzBuzz(3)).toEqual('Fizz');
+  });
 
-  test("returns Buzz when passed a multiple of 5", () => {});
+  test("returns Buzz when passed a multiple of 5", () => {
+    expect(fizzBuzz(5)).toEqual('Buzz');
+    expect(fizzBuzz(20)).toEqual('Buzz');
+    expect(fizzBuzz(55)).toEqual('Buzz');
+    expect(fizzBuzz(5000)).toEqual('Buzz');
+  });
 
-  test("returns FizzBuzz when passed a multiple 3 and 5", () => {});
+  test("returns FizzBuzz when passed a multiple 3 and 5", () => {
+    expect(fizzBuzz(15)).toEqual('FizzBuzz');
+    expect(fizzBuzz(30)).toEqual('FizzBuzz');
+    expect(fizzBuzz(150)).toEqual('FizzBuzz');
+    expect(fizzBuzz(150)).toEqual('FizzBuzz');
+  });
 
-  test("returns the number when it isn't a multiple of 3 or 5", () => {});
+  test("returns the number when it isn't a multiple of 3 or 5", () => {
+    expect(fizzBuzz(4)).toEqual(4);
+    expect(fizzBuzz(13)).toEqual(13);
+    expect(fizzBuzz(34)).toEqual(34);
+    expect(fizzBuzz(401)).toEqual(401);
+  });
 });
