c02475e60f06326800104b09817bfd1f64e24e06
Paul Beard <pbeardey@gmail.com>
2022-02-26 11:03:37 +0000

    test written

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,12 @@
+const { TestScheduler } = require("jest");
 const { humanCatDogYears } = require("../src");
 
+describe ("humanCatDogYears", () => {
+    test('takes an age in human years and returns an array [human years, cat years, dog years]', () => {
+        expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+        expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+        expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
+        expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    });
+});
 // Look Ma, no handlebars!!!
