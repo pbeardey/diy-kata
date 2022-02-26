82d0d903c7fa2362ef8b1b2d45be8c45ed60f1c2
Paul Beard <pbeardey@gmail.com>
2022-02-26 15:42:42 +0000

    tests written, tests passed

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,10 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    const names = namesObj.map(e => e.name);
+    const lastName = names.pop();
+    joinedNames = names.join(', ');
+    return `${joinedNames} & ${lastName}`;
+
+
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,8 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}])).toEqual('Bart, Lisa & Maggie');
+    expect(joinNames([{name: 'Fred'},{name: 'Wilma'},{name: 'Pebbles'},{name: 'Baby Puss'},{name: 'Grandma Pearl'},{name: 'Uncle Tex'}])).toEqual('Fred, Wilma, Pebbles, Baby Puss, Grandma Pearl & Uncle Tex');
+  });
 });
