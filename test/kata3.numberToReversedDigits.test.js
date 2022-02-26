7810ffed52929dfa2f34d9ad1a9059c258708bf6
Paul Beard <pbeardey@gmail.com>
2022-02-26 10:14:14 +0000

    tests written

--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,9 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(123)).toEqual([3,2,1]);
+    expect(numberToReversedDigits(1)).toEqual([1]);
+    expect(numberToReversedDigits(9823)).toEqual([2,3,8,9]);
+  });
 });
