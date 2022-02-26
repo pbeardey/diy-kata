34cf2f1e40788a651682521a8c88b4f75a4e0dc2
Paul Beard <pbeardey@gmail.com>
2022-02-26 16:08:57 +0000

    clean up: do separate const for each array to aid readablility

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -2,7 +2,9 @@ const { joinNames } = require("../src");
 
 describe("joinNames", () => {
   test("returns string of names, seperated by commas and an ampersand", () => {
-    expect(joinNames([{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}])).toEqual('Bart, Lisa & Maggie');
-    expect(joinNames([{name: 'Fred'},{name: 'Wilma'},{name: 'Pebbles'},{name: 'Baby Puss'},{name: 'Grandma Pearl'},{name: 'Uncle Tex'}])).toEqual('Fred, Wilma, Pebbles, Baby Puss, Grandma Pearl & Uncle Tex');
+    const names1 = [{name: 'Bart'},{name: 'Lisa'},{name: 'Maggie'}];
+    const names2 = [{name: 'Fred'},{name: 'Wilma'},{name: 'Pebbles'},{name: 'Baby Puss'},{name: 'Grandma Pearl'},{name: 'Uncle Tex'}];
+    expect(joinNames(names1)).toEqual('Bart, Lisa & Maggie');
+    expect(joinNames(names2)).toEqual('Fred, Wilma, Pebbles, Baby Puss, Grandma Pearl & Uncle Tex');
   });
 });
