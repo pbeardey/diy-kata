15de41179323cadfcbb5c425f423ed4d212e37a3
Paul Beard <pbeardey@gmail.com>
2022-02-26 12:44:27 +0000

    test 2 passed

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,5 +1,12 @@
-const reachDestination = (distance, speed) => {
-    return `I should be there in ${distance/speed} hours.`;
+const reachDestination = (distance, speed) => { 
+    let time = distance/speed;
+    time = (time * 10);
+    if (time % 5 !== 0){
+        time = Math.trunc(time/5)*5 + 5;
+    }
+    time = (time / 10);
+
+    return `I should be there in ${time} hours.`;
 };
 
 
