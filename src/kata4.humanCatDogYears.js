122656b186dc3eda9778cc1c6db32a89719d8652
Paul Beard <pbeardey@gmail.com>
2022-02-26 11:28:08 +0000

    test 3 passed

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,6 +1,12 @@
 const humanCatDogYears = number => {
     if (number === 1) {return [1,15,15];}
     else if (number === 2) {return [2,24,24];}
+    else if (number > 2) {
+        const twoLess = number - 2;
+        const catAge = 24 + twoLess * 4;
+        const dogAge = 24 + twoLess * 5;
+        return [ number, catAge, dogAge ];
+    }
 };
 
 module.exports = humanCatDogYears;
