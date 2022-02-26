d3861df4f67c713547068b93ebee5bb251d37132
Paul Beard <pbeardey@gmail.com>
2022-02-26 11:17:20 +0000

    test separated out to three tests

--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -2,9 +2,14 @@ const { TestScheduler } = require("jest");
 const { humanCatDogYears } = require("../src");
 
 describe ("humanCatDogYears", () => {
-    test('takes an age in human years and returns an array [human years, cat years, dog years]', () => {
+    test('takes an age in human years of 1 and returns an array of human age 1, cat age 15 and dog 15', () => {
         expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+
+    });
+    test('takes an age in human years of 2 and returns an array of human age 2, cat age 24 and dog 24', () => {
         expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    });
+    test('takes an age in human years and returns an array [human years, cat years, dog years]', () => {
         expect(humanCatDogYears(3)).toEqual([3, 28, 29]);
         expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
     });
