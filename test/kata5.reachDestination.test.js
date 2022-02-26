d00e8e41811a6fc6fd4e110d52a18d195928737a
Paul Beard <pbeardey@gmail.com>
2022-02-26 12:37:17 +0000

    test 2 fixed

--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -5,8 +5,9 @@ describe("reachDestination", () => {
     expect(reachDestination(45,10)).toEqual('I should be there in 4.5 hours.');
     expect(reachDestination(55,10)).toEqual('I should be there in 5.5 hours.');
   });
-  xit("returns string with estimated time of arrival rounded up to nearest 0.5", () => {
+  
+  it("returns string with estimated time of arrival rounded up to nearest 0.5", () => {
     expect(reachDestination(44,10)).toEqual('I should be there in 4.5 hours.');
-    expect(reachDestination(48,10)).toEqual('I should be there in 4.5 hours.');
+    expect(reachDestination(48,10)).toEqual('I should be there in 5 hours.');
   });
 });
